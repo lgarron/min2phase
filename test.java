b911816cbafb3f3e18a317f30907ea98643cb704
Lucas Garron <lucasg@gmx.de>
2012-09-15 06:47:37 -0700

    Variable max depth. 25 appears to be a lot better than 20 for Objective C on my computer.

--- test.java
+++ test.java
@@ -5,8 +5,8 @@ public class test {
 
       private static Search search = new Search();
 
-      public static void test(String cubeString) {
-            String result = search.solution(cubeString, 20, 1000000, 0, 0);
+      public static void test(String cubeString, int maxDepth) {
+            String result = search.solution(cubeString, maxDepth, 1000000, 0, 0);
             System.out.println(result);
       }
 
@@ -17,15 +17,17 @@ public class test {
             System.out.println("[Initialized]");
             
             System.out.println("[The first 3 test cases should be fast.]");
-            test("LURBUBDDBUUBDRFFFDRLLDFRRBDFDRBDRDULULBFLFFRUULFUBLBRL");
-            test("BRRBULFUBUFURRLFURDFLFFDFBDLURBDLDLBDDLRLDLRUFFRBBUUDB");
-            test("DBUBUFBRRDDFLRRLLLLBFRFDULFBFUFDUBBULLDRLDRURRDFFBUBUD");
+            test("LURBUBDDBUUBDRFFFDRLLDFRRBDFDRBDRDULULBFLFFRUULFUBLBRL", 20);
+            test("BRRBULFUBUFURRLFURDFLFFDFBDLURBDLDLBDDLRLDLRUFFRBBUUDB", 20);
+            test("DBUBUFBRRDDFLRRLLLLBFRFDULFBFUFDUBBULLDRLDRURRDFFBUBUD", 20);
             
-            System.out.println("[4th test case should take slightly longer.]");
-            test("LBRUUUUFRDLUFRDUULFRFFFDLDRFBBRDBRUBBFLLLLDBDFLURBDDRB");
+            System.out.println("[4th test case should take slightly longer the second time.]");
+            test("LBRUUUUFRDLUFRDUULFRFFFDLDRFBBRDBRUBBFLLLLDBDFLURBDDRB", 25);
+            test("LBRUUUUFRDLUFRDUULFRFFFDLDRFBBRDBRUBBFLLLLDBDFLURBDDRB", 20);
 
-            System.out.println("[5th test case should take significantly longer.]");
-            test("FDDRURLURUFFBRBRUFBRBFFLBUDLBBFDLUDRRDULLDLUDLBDRBFULF");
+            System.out.println("[5th test case should take significantly longer the second time.]");
+            test("FDDRURLURUFFBRBRUFBRBFFLBUDLBBFDLUDRRDULLDLUDLBDRBFULF", 25);
+            test("FDDRURLURUFFBRBRUFBRBFFLBUDLBBFDLUDRRDULLDLUDLBDRBFULF", 20);
 
             System.out.println("[Done]");
 	}
