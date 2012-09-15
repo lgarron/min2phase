511b7c859edd0cf6c6f6b9debea254cbcee6134a
Lucas Garron <lucasg@gmx.de>
2012-09-15 07:07:00 -0700

    Better testing.

--- test.java
+++ test.java
@@ -6,28 +6,31 @@ public class test {
       private static Search search = new Search();
 
       public static void test(String cubeString, int maxDepth) {
-            String result = search.solution(cubeString, maxDepth, 1000000, 0, 0);
+
+            int timeOut = 1000000;
+            int timeMin = 0;
+            int verbose = 0;
+
+            String result = search.solution(cubeString, maxDepth, timeOut, timeMin, verbose);
+
             System.out.println(result);
+
       }
 
 	public static void main(String[] args) {
 
-            System.out.println("[Starting]");
+            System.out.println("[Initializing]");
+            Tools.init();
 
-            System.out.println("[Initialized]");
-            
-            System.out.println("[The first 3 test cases should be fast.]");
-            test("LURBUBDDBUUBDRFFFDRLLDFRRBDFDRBDRDULULBFLFFRUULFUBLBRL", 20);
-            test("BRRBULFUBUFURRLFURDFLFFDFBDLURBDLDLBDDLRLDLRUFFRBBUUDB", 20);
-            test("DBUBUFBRRDDFLRRLLLLBFRFDULFBFUFDUBBULLDRLDRURRDFFBUBUD", 20);
-            
-            System.out.println("[4th test case should take slightly longer the second time.]");
-            test("LBRUUUUFRDLUFRDUULFRFFFDLDRFBBRDBRUBBFLLLLDBDFLURBDDRB", 25);
-            test("LBRUUUUFRDLUFRDUULFRFFFDLDRFBBRDBRUBBFLLLLDBDFLURBDDRB", 20);
+            System.out.println("[Testing random states with maxDepth 25]");
+            for (int i = 0; i < 10; i++){
+                  test(Tools.randomCube(), 25);
+            }
 
-            System.out.println("[5th test case should take significantly longer the second time.]");
-            test("FDDRURLURUFFBRBRUFBRBFFLBUDLBBFDLUDRRDULLDLUDLBDRBFULF", 25);
-            test("FDDRURLURUFFBRBRUFBRBFFLBUDLBBFDLUDRRDULLDLUDLBDRBFULF", 20);
+            System.out.println("[Testing random states with maxDepth 20]");
+            for (int i = 0; i < 10; i++){
+                  test(Tools.randomCube(), 20);
+            }
 
             System.out.println("[Done]");
 	}
